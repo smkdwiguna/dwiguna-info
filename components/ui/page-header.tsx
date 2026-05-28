98c83b29d9ecd89c6f7b0dbe7c0a6b05e5881462
Qeis Ismail <qeis926@gmail.com>
2026-05-29 01:15:42 +0700

    fix: update useRouter import from 'next/router' to 'next/navigation'

--- components/ui/page-header.tsx
+++ components/ui/page-header.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
 import { Button } from "./button";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export function PageHeaderBack({
 	onClick,
